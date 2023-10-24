fc1266bc524dd4a56f1a02ba7f9f50944f4e0879
dudapereirapmp <127161056+dudapereirapmp@users.noreply.github.com>
2023-10-23 21:52:32 -0300

    alterando codigo

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ function somTocaGrilo(){
     document.querySelector("som_tecla_grilo").play();}
     document.querySelector("tecla_grilo").onclick
     =somTocaGrilo;
+    const listdeteclas = document.querySelectorAll(".tecla");
+    listdeteclas[4].onclick=somTocaGrilo;
